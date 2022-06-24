16052264d37e3ab0e4dd7581588d8ea15a3adcd5
Saad Baig <sbaigcr7@gmail.com>
2022-06-24 21:01:20 +0530

    font-fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const expressLayout = require('express-ejs-layouts');
+// require('dotenv').config();
 const port = process.env.PORT || 5000;
 //init
 const app = express();
